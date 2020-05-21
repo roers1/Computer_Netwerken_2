1fcad4ec6859da37c2f3bab9f49b32eecafb067f
Ruben van Oers <r.vanoers1@student.avans.nl>
2020-05-21 12:46:24 +0200

    Opening imap inbox added

--- Course_4_Email_Tool/Inbox.cs
+++ Course_4_Email_Tool/Inbox.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using MailKit;
 using MailKit.Net.Imap;
+using MailKit.Security;
 
 namespace Course_4_Email_Tool
 {
@@ -17,6 +18,7 @@ namespace Course_4_Email_Tool
 	{
 		private readonly ImapClient _ic;
 		private readonly EmailConfiguration _emailConfiguration;
+		private int mailsToShow;
 
 		public Inbox(ImapClient ic, EmailConfiguration ec)
 		{
@@ -25,6 +27,35 @@ namespace Course_4_Email_Tool
 			_emailConfiguration = ec;
 
 			Label_LoggedInAs.Text = ec.ImapUsername;
+
+			ic.Inbox.Open(FolderAccess.ReadOnly);
+
+			mailsToShow = ic.Inbox.Count > 10 ? 10 : ic.Inbox.Count();
+
+			try
+			{
+				var dt = new DataTable("Inbox");
+				dt.Columns.Add("Subject", typeof(string));
+				dt.Columns.Add("Sender", typeof(string));
+				dt.Columns.Add("Body", typeof(string));
+				dt.Columns.Add("Date", typeof(string));
+
+				dataGridView1.DataSource = dt;
+
+				for (int i = 0; i < mailsToShow; i++)
+				{
+					var message = ic.Inbox.GetMessage(i);
+					dt.Rows.Add(new object[]
+					{
+						message.Subject, message.From, message.HtmlBody, message.Date.ToLocalTime()
+					});
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.Write(ex.Message);
+			}
+			
 		}
 
 		private void button_settings_Click(object sender, EventArgs e)
@@ -39,7 +70,7 @@ namespace Course_4_Email_Tool
 
 		private void button_sent_Click(object sender, EventArgs e)
 		{
-			Form write = new Write(_ic,_emailConfiguration);
+			Form write = new Write(_ic, _emailConfiguration);
 			write.Show();
 			write.Location = this.Location;
 			write.StartPosition = FormStartPosition.Manual;
@@ -48,7 +79,6 @@ namespace Course_4_Email_Tool
 
 		private void button_refresh_Click(object sender, EventArgs e)
 		{
-
 		}
 	}
 }
